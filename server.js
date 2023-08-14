ab3567aa8090faf54804a56cd07106c419a49f54
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2023-08-14 07:29:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 
-const path = require('path')
+// const path = require('path')
 
-const fs = require('fs')
+// const fs = require('fs')
 
 const bodyParser = require('body-parser')
 
